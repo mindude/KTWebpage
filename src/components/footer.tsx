ee7aa2e488954b0f770de476acb68b6645d51d73
mindude <edude0j9@gmail.com>
2018-07-15 10:41:06 -0700

    Setup finished

---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import {connect} from 'react-redux';
+import {Panel, Col, Row, Well, Button, ButtonGroup, Label, Modal} from 'react-bootstrap';
+import {bindActionCreators} from 'redux';
+
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {connect} from 'react-redux';
+import {Panel, Col, Row, Well, Button, ButtonGroup, Label, Modal, Nav, NavItem, Navbar, Badge} from 'react-bootstrap';
+import {bindActionCreators} from 'redux';
+
+class Header extends React.Component{
+	
+
+}
